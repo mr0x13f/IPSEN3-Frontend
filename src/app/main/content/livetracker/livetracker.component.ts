2b9441557873abc284fed6d70017176946d41725
0x13f <twarmerdam14@gmail.com>
2020-01-09 14:36:55 +0100

    fixed breaking everything

--- src/app/main/content/livetracker/livetracker.component.ts
+++ src/app/main/content/livetracker/livetracker.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { toBase64String } from '@angular/compiler/src/output/source_map';
-import { HttpService } from 'src/services/http-service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-livetracker',
@@ -12,7 +11,7 @@ export class LivetrackerComponent implements OnInit {
   isTracking: boolean = false;
 
   constructor(
-    private httpService:HttpService
+    private authService:AuthService
   ) { }
 
   ngOnInit() {
@@ -29,11 +28,11 @@ export class LivetrackerComponent implements OnInit {
   }
 
   startTracking() {
-    // start tracking
+  
   }
 
   stopTracking() {
-    // stop tracking
+
   }
 
 }
