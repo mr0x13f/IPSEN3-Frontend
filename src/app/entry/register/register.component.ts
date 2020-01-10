55a62ed13654a3715a83b7d3ae5be5de087eea8c
Timvanhal <s1115001@student.hsleiden.nl>
2020-01-10 10:23:42 +0100

    Update register.component.ts

--- src/app/entry/register/register.component.ts
+++ src/app/entry/register/register.component.ts
@@ -39,7 +39,7 @@ export class RegisterComponent implements OnInit {
 
                           
     console.log(user.email + '\t' + user.name + '\t' + user.password);
-  
+    
     if( this.confirmPasswordInputRef.nativeElement.value === this.confirmPasswordInputRef.nativeElement.value){
     }else{
       //todo Feedback geven
