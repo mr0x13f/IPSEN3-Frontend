18bf9213e70d775e8a4d33d8c574465c22626cb0
0x13f <twarmerdam14@gmail.com>
2020-01-27 11:06:19 +0100

    livetracker en resetpassword mischien gefixt

--- src/app/main/content/livetracker/livetracker.component.ts
+++ src/app/main/content/livetracker/livetracker.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { toBase64String } from '@angular/compiler/src/output/source_map';
 import { HttpService } from 'src/app/services/http.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { clearInterval } from 'timers';
 
 @Component({
   selector: 'app-livetracker',
   templateUrl: './livetracker.component.html',
   styleUrls: ['./livetracker.component.css']
 })
-export class LivetrackerComponent implements OnInit {
+export class LivetrackerComponent implements OnInit, OnDestroy {
 
   isTracking: boolean = false;
+  interval;
+  coordinatesList: Coordinates[] = [];
+  locationInterval = 10 * 1000;
 
   constructor(
     private authService:AuthService
@@ -30,10 +34,58 @@ export class LivetrackerComponent implements OnInit {
   }
 
   startTracking() {
+
+    this.saveLocation();
+    
+    this.interval = setInterval( () => { this.saveLocation(); }, this.locationInterval);
   
   }
 
+  saveLocation() {
+      
+    navigator.geolocation.getCurrentPosition((position) => {
+        this.coordinatesList.push(position.coords);
+    })
+
+  }
+
   stopTracking() {
+
+    clearInterval(this.interval);
+
+    console.log(this.calculateTotalDistance());
+
+  }
+
+  calculateTotalDistance() {
+
+    let distance = 0;
+
+    for (let i = 1; i<this.coordinatesList.length; i++) {
+
+       let coord1 = this.coordinatesList[i-1];
+       let coord2 = this.coordinatesList[i];
+       distance += this.calculateDistance(coord1, coord2);
+
+    }
+
+    return distance;
+
+  }
+
+  calculateDistance(coord1:Coordinates, coord2:Coordinates) {
+    var p = 0.017453292519943295;
+    var c = Math.cos;
+    var a = 0.5 - c((coord2.latitude - coord1.latitude) * p)/2 + 
+            c(coord1.latitude * p) * c(coord2.latitude * p) * 
+            (1 - c((coord2.longitude - coord1.longitude) * p))/2;
+  
+    return 12742 * Math.asin(Math.sqrt(a));
+  }
+
+  ngOnDestroy() {
+
+    this.stopTracking() 
 
   }
 
